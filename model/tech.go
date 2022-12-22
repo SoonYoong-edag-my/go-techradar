112cc38819948c02a51858b52df6dae40850e027
soon-yoong <soon.yoon-ooi@edag.com.my>
2022-12-22 15:52:46 +0800

    Added optional description field

--- model/tech.go
+++ model/tech.go
@@ -25,12 +25,13 @@ const (
 )
 
 type Tech struct {
-	Id       uint     `json:"id" gorm:"primary_key"`
-	Category Category `json:"category" binding:"required" validate:"oneof=Frontend Apps Backend Dev/Ops Consulting Application IT_Service"`
-	Status   Status   `json:"status" binding:"required" validate:"oneof=core assess hold adopt trial"`
-	Name     string   `json:"name" binding:"required"`
-	Active   bool     `json:"active"`
-	Moved    int8     `json:"moved"`
+	Id          uint     `json:"id" gorm:"primary_key"`
+	Category    Category `json:"category" binding:"required" validate:"oneof=Frontend Apps Backend Dev/Ops Consulting Application IT_Service"`
+	Status      Status   `json:"status" binding:"required" validate:"oneof=core assess hold adopt trial"`
+	Name        string   `json:"name" binding:"required"`
+	Description string   `json:"description"`
+	Active      bool     `json:"active"`
+	Moved       int8     `json:"moved"`
 }
 
 func (t *Tech) Validate() error {
